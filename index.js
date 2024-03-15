a505516e05d247c73a1c0416f71450cda24b0cd1
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-14 17:20:38 -0700

    implemented ISS times for my location

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:', ip);
-// });
-
-// fetchCoordsByIP('154.20.168.220', (err, data) => {
-//   if (err) console.log(err.message);
-//   else console.log(data);
-// });
-
-fetchISSFlyOverTimes(
-  { latitude: '49.27670', longitude: '-123.13000' },
-  (err, data) => {
-    if (err) console.log(err.message);
-    else console.log(data);
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
   }
-);
+  // success, print out the deets!
+  passTimes.forEach(({ risetime, duration }) =>
+    console.log(
+      `Next pass at ${new Date(Date.now() + risetime)} for ${duration} seconds!`
+    )
+  );
+});
